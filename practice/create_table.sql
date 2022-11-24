9a8276b808d67096cc8403c8e155c030da306152
Soham1012 <sohamp101202@gmail.com>
2022-11-24 17:40:21 +0530

    create table

---
+++ practice/create_table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE practice;
+USE practice;
+
+/* new table impored
+table name = 'practice_table */ 
+
+/* crate new table*/
+
+CREATE TABLE student_details
+( std_id INT UNSIGNED ,
+std_name VARCHAR (15),
+std_stndard int ,
+std_age int ,
+std_add varchar(30),
+primary key (std_id));
+
+SELECT * FROM student_details;
+
+  
